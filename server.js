e3c707201106ba99e1b1f24ab7f59c2559011818
harshalyallewar <harshalyallewar2@gmail.com>
2023-01-14 10:51:54 +0530

    render

--- server.js
+++ server.js
@@ -19,14 +19,13 @@ app.use(
 );
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
   app.use(express.static("client/build"));
 
-  const path = require("path");
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 app.listen(PORT, () => {});
